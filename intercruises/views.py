0d0b6df5b77f6ed5102922735294e38989c7c798
Luicko <luiko.gmolina@gmail.com>
2017-09-24 04:58:22 +0100

    sdsd

--- intercruises/views.py
+++ intercruises/views.py
@@ -242,7 +242,7 @@ def delcompany(companyname):
 
 @app.route('/eliminateassign/<cruise_id>/<date>', methods=['GET', 'POST'])
 def eliminateassign(cruise_id, date):
-    date = datetime.strptime(form.date.data, '%d/%M/%Y').date()
+    date = datetime.strptime(date, '%d/%M/%Y').date()
     assign = GuideCruises.query.filter_by(cruise_id=cruise_id, date=date).first_or_404()
     cruise = Cruises.query.filter_by(cruise_id=assign.cruise_id).first_or_404()
     form = ElimAss()
